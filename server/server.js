4aa45166ffbe9beecaaa8702155ea8ecde1ea3fd
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-15 18:49:22 +0530

    basic imports to make authentication and fet data in json is done

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API WORKING!")
